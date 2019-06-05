07398273ecd8fcf86f0ef7e13f544d1721d1b038
Olga Rosas <olga.rosas@gmail.com>
2019-06-05 17:06:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,8 +65,6 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
-
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -82,8 +80,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto][:known_for]
-#{}"Ruby", :languages => ["LISP", "C"]}
+		 
+programmer_hash[:yukihiro_matsumoto] = {}
+programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+
 
 end
 
